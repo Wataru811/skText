7fdd840874515688bd9446fcf6655ac363897ca6
wash <wash.fuel@mgail.com>
2020-12-28 21:47:12 +0700

    update v1.1

--- skText.py
+++ skText.py
@@ -1,30 +1,44 @@
 #!/usr/bin/python
 
 
-# skText.py
-# extract text string from  sketch file
+# skText.py  
+# extract text string from	sketch file
 # $ python skText page.json 
 #
+# v1.0 extract 1st level strings
+# v1.1 extranct nexted strings
+#
+
 
 
 import json
 import sys
 
 
+allFlag = False
+argFile = 1
 
-# check  argument
+# check	argument
 arguments = len(sys.argv) 
 if( arguments < 2 ): 
 	print( "Invalide argument.\n\n" )
 	print( "## ------------------------------" )
-	print( "## Sketch text string extractor" )
+	print( "## skText / Sketch text string extractor (v1.1)" )
 	print( "## written by W.Ishizuka" )
-	print( "$ skText <sketch-page-json>\n\n" )
+	print( "$ skText [-all] <sketch-page-json>\n\n" )
+	print( "option:" )
+	print( "   -all : print nexted strings" );
+	print( "   (default: print only top level strings)" );
 	exit(0)
+
+if( arguments == 3 ): 
+	if sys.argv[1].find( "-all" ) == 0:
+		allFlag = True
+		argFile = 2
 
 ## input
 try:
-	f = open( sys.argv[1], 'r')
+	f = open( sys.argv[argFile], 'r')
 except OSError as e:
 	print(e)
 	exit(0)
@@ -39,16 +53,45 @@ except e:
 ## print('json_dict:{}'.format(type(json_dict)))
 ly = json_dict["layers"]
 res = []
+skipstr = [ "step", "Group", "lv" ,"Lv", "Rectangle", "btn", "copy","overlay","Path","segmantControl","Bar" ];
+result = {}
+
 for item in ly:
+	lbl = ""
 	if "attributedString" in item:
-		res.append( item["attributedString"]["string"] )
+		lbl = item["attributedString"]["string"]
 	else:
-		res.append( item["name"] )
+		lbl = item["name"] 
+	res.append(lbl )
+	lbl2=""
+	result[ lbl ] = []
+	if "layers" in item:
+		subLy = item["layers"]
+		for subItem in subLy:
+			if "attributedString" in subItem:
+				lbl2 = subItem["attributedString"]["string"]
+			else:
+				lbl2 = subItem["name"]
+			flg = True;
+			for ss in skipstr:
+				if lbl2.find( ss ) == 0:
+					flg =False 
+					break
+			if flg  :
+				result[lbl].append( lbl2 );
 res = sorted(res) ;
 
 ## output
-for ss in res:
-	print( ss );
+if allFlag: 
+	for ss in res:
+		print( "\n\n"+ ss + "\n----------------------" )
+		ll = result[ss]
+		for str in ll:
+			print( str )	
+else:
+	for ss in res:
+		print( ss )	
+
 
 
 
